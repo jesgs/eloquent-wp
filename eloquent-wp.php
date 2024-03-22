ca8c6ceefa85313d507b2a59291e4f2d473a2a5d
jesgs <2304490+jesgs@users.noreply.github.com>
2024-03-22 17:03:50 -0500

    Initial project commit

---
+++ eloquent-wp.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Eloquent WP
+ * Plugin URI:      https://www.jesgs.com/eloquent-wp
+ * Description:     A wrapper for WPDB using Laravel Eloquent
+ * Author:          Jess Green
+ * Author URI:      https://www.jesgs.com
+ * Text Domain:     eloquent-wp
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Eloquent_Wp
+ */
+
+require 'vendor/autoload.php';
