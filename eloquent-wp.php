089c56764fb06d31d56fa6f7641f99ee0ae54641
jesgs <2304490+jesgs@users.noreply.github.com>
2024-03-22 17:57:47 -0500

    Add initial code

--- eloquent-wp.php
+++ eloquent-wp.php
@@ -12,4 +12,22 @@
  * @package         Eloquent_Wp
  */
 
-require 'vendor/autoload.php';
+$eloquent_wp_plugin_folder = plugin_basename( __DIR__ );
+
+if ( ! defined( 'ELOQUENT_WP_VERSION' ) ) {
+	define( 'ELOQUENT_WP_VERSION', '3.5.0' );
+}
+
+if ( ! defined( 'ELOQUENT_WP_FOLDER' ) ) {
+	define( 'ELOQUENT_WP_FOLDER', $eloquent_wp_plugin_folder );
+}
+
+if ( ! defined( 'ELOQUENT_WP_ABSPATH' ) ) {
+	define( 'ELOQUENT_WP_ABSPATH', plugin_dir_path( __FILE__ ) );
+}
+
+if ( ! defined( 'ELOQUENT_WP_URLPATH' ) ) {
+	define( 'ELOQUENT_WP_URLPATH', plugin_dir_url( __FILE__ ) );
+}
+
+require ELOQUENT_WP_ABSPATH . 'vendor/autoload.php';
